337917854b3bdcead285e72daf173e807db46b5b
Mariia Ustilkina <mariia.ustilkina@gofore.com>
2019-10-03 14:46:42 +0300

    Set up frontend

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+`;
+
+
+const App: React.FC = () => {
+  return (
+    <Wrapper>
+    </Wrapper>
+  );
+}
+
+export default App;
