8cab4aee9641ef98caa7dfc30c46bbb4a4785bbd
liam.miller@gofore.com <liam.miller@gofore.com>
2019-10-07 19:05:29 +0100

    Provided e2e testing.

--- backend/src/admin/admin.service.spec.ts
+++ backend/src/admin/admin.service.spec.ts
@@ -1,13 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminService } from './admin.service';
+import { Repository, createConnection, Connection } from 'typeorm';
+import { Admin } from './admin.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('AdminService', () => {
   let service: AdminService;
 
   beforeEach(async () => {
+    const connection = await createConnection({
+      type: 'sqlite',
+      database: './testdb.sql',
+      entities: ['src/entities/*.entity{.ts,.js}'],
+      synchronize: true,
+    });
     const module: TestingModule = await Test.createTestingModule({
       providers: [AdminService],
-    }).compile();
+    })
+      .overrideProvider(Connection)
+      .useValue(connection)
+      .compile();
 
     service = module.get<AdminService>(AdminService);
   });
