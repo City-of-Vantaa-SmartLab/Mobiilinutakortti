d766a54604eca2aee51d6999ba776178187422db
liam.miller@gofore.com <liam.miller@gofore.com>
2019-10-08 14:03:31 +0100

    Secured secret for JWT in prod.

--- backend/src/authentication/authentication.consts.ts
+++ backend/src/authentication/authentication.consts.ts
@@ -1,2 +1,5 @@
 export const saltRounds = 10;
-export const jwt = { secret: 'Remember to make me more secure before prod!', expiry: `7d` };
+export const jwt = {
+    secret: process.env.NODE_ENV === 'production' ? process.env.JWT : 'Remember to make me more secure before prod!',
+    expiry: `7d`,
+};
