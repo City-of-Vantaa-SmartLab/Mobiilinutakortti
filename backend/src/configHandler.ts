689608969be8e81e546296134805ad016ebf40ff
liam.miller@gofore.com <liam.miller@gofore.com>
2019-10-09 10:14:31 +0100

    Made config handler cleaner.

--- backend/src/configHandler.ts
+++ backend/src/configHandler.ts
@@ -7,34 +7,19 @@ export class ConfigHelper {
     }
 
     static getJWTSecret(): string {
-        return this.isLive() ? process.env.JWT : 'Remember to make me more secure before prod!';
+        return process.env.JWT || 'Remember to make me more secure before prod!';
     }
 
     static getDatabaseConnection(): TypeOrmModuleOptions {
-        let dbOptions: TypeOrmModuleOptions;
-        if (this.isLive()) {
-            dbOptions = {
-                type: 'postgres',
-                host: process.env.RDS_HOSTNAME,
-                port: +process.env.RDS_PORT,
-                username: process.env.RDS_USERNAME,
-                password: process.env.RDS_PASSWORD,
-                database: 'vantaa-pwa',
-                entities: ['dist/**/*.entity{.ts,.js}'],
-                synchronize: true,
-            };
-        } else {
-            dbOptions = {
-                type: 'postgres',
-                host: 'db',
-                port: 5432,
-                username: 'postgres',
-                password: 'password',
-                database: 'nuta',
-                entities: ['dist/**/*.entity{.ts,.js}'],
-                synchronize: true,
-            };
-        }
-        return dbOptions;
+        return {
+            type: 'postgres',
+            host: process.env.RDS_HOSTNAME || 'db',
+            port: +process.env.RDS_PORT || 5432,
+            username: process.env.RDS_USERNAME || 'postgres',
+            password: process.env.RDS_PASSWORD || 'password',
+            database: process.env.DB_NAME || 'nuta',
+            entities: ['dist/**/*.entity{.ts,.js}'],
+            synchronize: true,
+        };;
     }
 }
