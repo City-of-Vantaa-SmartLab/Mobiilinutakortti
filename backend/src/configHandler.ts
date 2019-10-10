0c6dacbff0a1d4915f371a8a3e3d8dd6b057e954
liam.miller@gofore.com <liam.miller@gofore.com>
2019-10-10 08:02:33 +0100

    Fixes for enviromental variable miss name.

--- backend/src/configHandler.ts
+++ backend/src/configHandler.ts
@@ -17,7 +17,7 @@ export class ConfigHelper {
             port: +process.env.RDS_PORT || 5432,
             username: process.env.RDS_USERNAME || 'postgres',
             password: process.env.RDS_PASSWORD || 'password',
-            database: process.env.DB_NAME || 'nuta',
+            database: process.env.RDS_DB_NAME || 'nuta',
             entities: ['dist/**/*.entity{.ts,.js}'],
             synchronize: true,
         };;
