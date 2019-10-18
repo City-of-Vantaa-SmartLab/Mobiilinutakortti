0138f080cb4e34973583e51245ff62ad176fcdd1
Mikael Harju <mikael.harju@gofore.com>
2019-10-18 16:59:06 +0300

    Add Finnish titles and reorganise create fields

--- admin-frontend/src/components/junior.js
+++ admin-frontend/src/components/junior.js
@@ -23,8 +23,12 @@ const genderChoices = [
     { id: 'o', name: 'Muu' }
 ];
 
+const JuniorEditTitle = ({ record }) => (
+    <span>{`Muokkaa ${record.firstName} ${record.lastName}`}</span>
+);
+
 export const JuniorList = (props) => (
-    <List {...props}>
+    <List title="Nuoret" {...props}>
         <Datagrid>
             <TextField label="Puhelinnumero" source="phoneNumber" />
             <FunctionField label="Nimi" render={record => `${record.firstName} ${record.lastName}`} />
@@ -38,18 +42,18 @@ export const JuniorList = (props) => (
     </List>
 );
 export const JuniorCreate = (props) => (
-    <Create {...props}>
+    <Create title="Rekisteröi nuori" {...props}>
         <TabbedForm>
-            <FormTab label="Junior">
-                <TextInput label="Puhelinnumero" source="phoneNumber" validate={ required() }/>
-                <TextInput label="Sukunimi" source="lastName" validate={ required() }/>
+            <FormTab label="Nuori">
                 <TextInput label="Etunimi" source="firstName" validate={ required() }/>
-                <SelectInput label="Sukupuoli" source="gender" choices={ genderChoices } validate={ [required(), choices(['m', 'f', 'o'])] } />
+                <TextInput label="Sukunimi" source="lastName" validate={ required() }/>
+                <TextInput label="Puhelinnumero" source="phoneNumber" validate={ required() }/>
                 <NumberInput label="Ikä" source="age" validate={ [required(), number()] }/>
+                <SelectInput label="Sukupuoli" source="gender" choices={ genderChoices } validate={ [required(), choices(['m', 'f', 'o'])] } />
                 <TextInput label="Kotinuorisotalo" source="homeYouthClub" validate={ required() }/>
                 <TextInput label="Postinumero" source="postCode" validate={ required() }/>
             </FormTab>
-            <FormTab label="Parent">
+            <FormTab label="Huoltaja">
                 <TextInput label="Huoltajan nimi" source="parentsName" validate={ required() }/>
                 <TextInput label="Huoltajan puhelinnumero" source="parentsPhoneNumber" validate={ required() }/>
             </FormTab>
