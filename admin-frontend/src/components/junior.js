3532d525dcd6eda4de3ac8da50ecdce415108957
Mikael Harju <mikael.harju@gofore.com>
2019-10-23 20:37:57 +0300

    Change field ordering

--- admin-frontend/src/components/junior.js
+++ admin-frontend/src/components/junior.js
@@ -7,8 +7,7 @@ import {
     NumberField,
     FunctionField,
     Create,
-    TabbedForm,
-    FormTab, 
+    SimpleForm,
     TextInput,
     SelectInput,
     NumberInput,
@@ -32,12 +31,12 @@ const JuniorEditTitle = ({ record }) => (
 export const JuniorList = (props) => (
     <List title="Nuoret" {...props}>
         <Datagrid>
-            <TextField label="Puhelinnumero" source="phoneNumber" />
             <FunctionField label="Nimi" render={record => `${record.firstName} ${record.lastName}`} />
             <SelectField label="Sukupuoli" source="gender" choices={genderChoices} />
             <NumberField label="Ikä" source="age" />
-            <TextField label="Kotinuorisotalo" source="homeYouthClub" />
+            <TextField label="Puhelinnumero" source="phoneNumber" />
             <TextField label="Postinumero" source="postCode" />
+            <TextField label="Kotinuorisotalo" source="homeYouthClub" />
             <TextField label="Huoltajan nimi" source="parentsName" />
             <TextField label="Huoltajan puhelinnumero" source="parentsPhoneNumber" />
             <EditButton />
@@ -46,39 +45,31 @@ export const JuniorList = (props) => (
 );
 export const JuniorCreate = (props) => (
     <Create title="Rekisteröi nuori" {...props}>
-        <TabbedForm>
-            <FormTab label="Nuori">
-                <TextInput label="Etunimi" source="firstName" validate={ required() }/>
-                <TextInput label="Sukunimi" source="lastName" validate={ required() }/>
-                <TextInput label="Puhelinnumero" source="phoneNumber" validate={ required() }/>
-                <NumberInput label="Ikä" source="age" validate={ [required(), number()] }/>
-                <SelectInput label="Sukupuoli" source="gender" choices={ genderChoices } validate={ [required(), choices(['m', 'f', 'o'])] } />
-                <TextInput label="Kotinuorisotalo" source="homeYouthClub" validate={ required() }/>
-                <TextInput label="Postinumero" source="postCode" validate={ required() }/>
-            </FormTab>
-            <FormTab label="Huoltaja">
-                <TextInput label="Huoltajan nimi" source="parentsName" validate={ required() }/>
-                <TextInput label="Huoltajan puhelinnumero" source="parentsPhoneNumber" validate={ required() }/>
-            </FormTab>
-        </TabbedForm>
+        <SimpleForm>
+            <TextInput label="Etunimi" source="firstName" validate={ required() }/>
+            <TextInput label="Sukunimi" source="lastName" validate={ required() }/>
+            <SelectInput label="Sukupuoli" source="gender" choices={ genderChoices } validate={ [required(), choices(['m', 'f', 'o'])] } />
+            <NumberInput label="Ikä" source="age" validate={ [required(), number()] }/>
+            <TextInput label="Puhelinnumero" source="phoneNumber" validate={ required() }/>
+            <TextInput label="Postinumero" source="postCode" validate={ required() }/>
+            <TextInput label="Kotinuorisotalo" source="homeYouthClub" validate={ required() }/>
+            <TextInput label="Huoltajan nimi" source="parentsName" validate={ required() }/>
+            <TextInput label="Huoltajan puhelinnumero" source="parentsPhoneNumber" validate={ required() }/>
+        </SimpleForm>
     </Create>
 );
 export const JuniorEdit = (props) => (
     <Edit title={<JuniorEditTitle />} {...props}>
-        <TabbedForm>
-            <FormTab label="Nuori">
-                <TextInput label="Etunimi" source="firstName" />
-                <TextInput label="Sukunimi" source="lastName" />
-                <TextInput label="Puhelinnumero" source="phoneNumber" />
-                <NumberInput label="Ikä" source="age" />
-                <SelectInput label="Sukupuoli" source="gender" choices={genderChoices} />
-                <TextInput label="Kotinuorisotalo" source="homeYouthClub" />
-                <TextInput label="Postinumero" source="postCode" />
-            </FormTab>
-            <FormTab label="Huoltaja">
-                <TextInput label="Huoltajan nimi" source="parentsName" />
-                <TextInput label="Huoltajan puhelinnumero" source="parentsPhoneNumber" />
-            </FormTab>
-        </TabbedForm>
+        <SimpleForm>
+            <TextInput label="Etunimi" source="firstName" />
+            <TextInput label="Sukunimi" source="lastName" />
+            <SelectInput label="Sukupuoli" source="gender" choices={genderChoices} />
+            <NumberInput label="Ikä" source="age" />
+            <TextInput label="Puhelinnumero" source="phoneNumber" />
+            <TextInput label="Postinumero" source="postCode" />
+            <TextInput label="Kotinuorisotalo" source="homeYouthClub" />
+            <TextInput label="Huoltajan nimi" source="parentsName" />
+            <TextInput label="Huoltajan puhelinnumero" source="parentsPhoneNumber" />
+        </SimpleForm>
     </Edit>
 );
