ba81a35834223a4baa42d616e3f54ffe6b5839ef
Mikael Harju <mikael.harju@gofore.com>
2019-11-05 17:13:14 +0200

    Add youthclub dropdown menu to youth edit and create

--- admin-frontend/src/components/junior.js
+++ admin-frontend/src/components/junior.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
     List,
     Datagrid,
@@ -16,13 +16,19 @@ import {
     choices,
     EditButton,
     Edit,
+    GET_LIST
  } from 'react-admin';
+
+ import { dataProvider } from '../providers/dataProvider';
 
 const genderChoices = [
     { id: 'm', name: 'Mies' },
     { id: 'f', name: 'Nainen' },
     { id: 'o', name: 'Muu' }
 ];
+
+const getYouthClubs = () => dataProvider(GET_LIST, 'youthClub').then((res) => res.data)
+    .then((res) => res.map((youthClub) => ({id: youthClub.name, name: youthClub.name}))) // TODO: Eventually it will be wise to change the ID to the actual ID. This will require backend changes.
 
 const JuniorEditTitle = ({ record }) => (
     <span>{`Muokkaa ${record.firstName} ${record.lastName}`}</span>
@@ -43,33 +49,56 @@ export const JuniorList = (props) => (
         </Datagrid>
     </List>
 );
-export const JuniorCreate = (props) => (
-    <Create title="Rekisteröi nuori" {...props}>
-        <SimpleForm redirect="list">
-            <TextInput label="Etunimi" source="firstName" validate={ required() }/>
-            <TextInput label="Sukunimi" source="lastName" validate={ required() }/>
-            <SelectInput label="Sukupuoli" source="gender" choices={ genderChoices } validate={ [required(), choices(['m', 'f', 'o'])] } />
-            <NumberInput label="Ikä" source="age" validate={ [required(), number()] }/>
-            <TextInput label="Puhelinnumero" source="phoneNumber" validate={ required() }/>
-            <TextInput label="Postinumero" source="postCode" validate={ required() }/>
-            <TextInput label="Kotinuorisotalo" source="homeYouthClub" validate={ required() }/>
-            <TextInput label="Huoltajan nimi" source="parentsName" validate={ required() }/>
-            <TextInput label="Huoltajan puhelinnumero" source="parentsPhoneNumber" validate={ required() }/>
-        </SimpleForm>
-    </Create>
+export const JuniorCreate = (props) => {
+    const [youthClubs, setYouthClubs] = useState([]);
+    useEffect(() => {
+        const addYouthClubsToState = async () => {
+            const youthClubs = await getYouthClubs();
+            setYouthClubs(youthClubs);
+        };
+        addYouthClubsToState();
+    }, [youthClubs]);
+
+    return (
+        <Create title="Rekisteröi nuori" {...props}>
+            <SimpleForm redirect="list">
+                <TextInput label="Etunimi" source="firstName" validate={ required() }/>
+                <TextInput label="Sukunimi" source="lastName" validate={ required() }/>
+                <SelectInput label="Sukupuoli" source="gender" choices={ genderChoices } validate={ [required(), choices(['m', 'f', 'o'])] } />
+                <NumberInput label="Ikä" source="age" validate={ [required(), number()] }/>
+                <TextInput label="Puhelinnumero" source="phoneNumber" validate={ required() }/>
+                <TextInput label="Postinumero" source="postCode" validate={ required() }/>
+                <SelectInput label="Kotinuorisotalo" source="homeYouthClub" choices={youthClubs} validate={ required() }/>
+                <TextInput label="Huoltajan nimi" source="parentsName" validate={ required() }/>
+                <TextInput label="Huoltajan puhelinnumero" source="parentsPhoneNumber" validate={ required() }/>
+            </SimpleForm>
+        </Create>
 );
-export const JuniorEdit = (props) => (
-    <Edit title={<JuniorEditTitle />} {...props} undoable={false}>
-        <SimpleForm>
-            <TextInput label="Etunimi" source="firstName" />
-            <TextInput label="Sukunimi" source="lastName" />
-            <SelectInput label="Sukupuoli" source="gender" choices={genderChoices} />
-            <NumberInput label="Ikä" source="age" />
-            <TextInput label="Puhelinnumero" source="phoneNumber" />
-            <TextInput label="Postinumero" source="postCode" />
-            <TextInput label="Kotinuorisotalo" source="homeYouthClub" />
-            <TextInput label="Huoltajan nimi" source="parentsName" />
-            <TextInput label="Huoltajan puhelinnumero" source="parentsPhoneNumber" />
-        </SimpleForm>
-    </Edit>
-);
+
+}
+export const JuniorEdit = (props) => {
+    const [youthClubs, setYouthClubs] = useState([]);
+    useEffect(() => {
+        const addYouthClubsToState = async () => {
+            const youthClubs = await getYouthClubs();
+            setYouthClubs(youthClubs);
+        };
+        addYouthClubsToState();
+    }, [youthClubs]);
+
+    return (
+        <Edit title={<JuniorEditTitle />} {...props} undoable={false}>
+            <SimpleForm>
+                <TextInput label="Etunimi" source="firstName" />
+                <TextInput label="Sukunimi" source="lastName" />
+                <SelectInput label="Sukupuoli" source="gender" choices={genderChoices} />
+                <NumberInput label="Ikä" source="age" />
+                <TextInput label="Puhelinnumero" source="phoneNumber" />
+                <TextInput label="Postinumero" source="postCode" />
+                <SelectInput label="Kotinuorisotalo" source="homeYouthClub" choices={youthClubs} validate={ required() }/>
+                <TextInput label="Huoltajan nimi" source="parentsName" />
+                <TextInput label="Huoltajan puhelinnumero" source="parentsPhoneNumber" />
+            </SimpleForm>
+        </Edit>
+    );
+}
