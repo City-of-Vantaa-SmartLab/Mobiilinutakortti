df751dffb0848a0cc62f06170b19b716564bec34
Riku Ruokonen <riku.ruokonen@gofore.com>
2020-03-20 16:06:22 +0200

    added utils dir with helper file including helper func to determine if given dates differ too much

--- backend/src/utils/helpers.ts
+++ backend/src/utils/helpers.ts
@@ -1,8 +1,8 @@
-export const datesMatchOnHourlyLevel = (d1: Date, d2: Date): boolean => {
+export const datesDifferLessThan = (d1: Date, d2: Date, maxDiff: number): boolean => {
     return (
         d1.getFullYear() === d2.getFullYear() &&
         d1.getMonth() === d2.getMonth() &&
         d1.getDay() === d2.getDay() &&
-        d1.getHours() === d2.getHours()
+        Math.abs(d1.getHours() - d2.getHours()) < maxDiff
     );
 };
