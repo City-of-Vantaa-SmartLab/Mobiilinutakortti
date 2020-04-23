09e7d4c9cf6bb74090334bab193e3ed1592b1e47
Riku Ruokonen <riku.ruokonen@gofore.com>
2020-04-23 11:50:26 +0300

    Fixed breaking error in duplicate check-in

--- admin-frontend/src/App.js
+++ admin-frontend/src/App.js
@@ -37,7 +37,7 @@ const App = () => {
                     }
                 })
             }
-        }, 1 * 60000);
+        }, 60000);
 
         return () => {
             clearInterval(validCheck);
