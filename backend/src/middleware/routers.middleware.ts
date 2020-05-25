8644e9bc192bf93cb5b6ddb12eda40a747e6090f
Olli Helin <olli.helin@gofore.com>
2020-05-25 19:34:00 +0300

    chore: add debug logging

--- backend/src/middleware/routers.middleware.ts
+++ backend/src/middleware/routers.middleware.ts
@@ -5,6 +5,18 @@ import * as content from '../content.json';
 @Injectable()
 export class RoutersMiddleware implements NestMiddleware {
     use(req: any, res: any, next: () => void) {
+        // TODO: remove these logs once the missing index.html error is solved.
+        try {
+          console.log("Middleware DEBUG:");
+          console.log("    - originalUrl: " + req.originalUrl);
+          console.log("    - header.host: " + req.headers.host);
+          console.log("    - header.ua: " + req.headers['user-agent']);
+          console.log("    - remote IP: " + req.connection.remoteAddress);
+        }
+        catch {
+          console.log("DEBUG: caught exception.");
+        }
+
         let { baseUrl } = req;
         baseUrl = baseUrl.toLowerCase();
         if (baseUrl.includes(content.Routes.api)) {
