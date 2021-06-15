43d6ec7a7feded290e41c9005fbde0149bde2a3f
Juho Karppinen <juho.karppinen@gofore.com>
2021-06-15 16:10:15 +0300

    feat: add menu item for starting a new season
    The menu item is visible only for super users. Clicking on the item
    opens up a view with additional information about starting a new season
    and a button which actually calls the backend for performing the
    operation.

--- admin-frontend/src/customRoutes.js
+++ admin-frontend/src/customRoutes.js
@@ -4,10 +4,15 @@ import CheckInView from './components/checkIn/checkIn';
 import LogBookView from './components/logbook';
 import LogBookListView from './components/logbookList';
 import ChangePasswordView from './components/changePassword';
+import NewSeason from './components/newSeason';
 
-export default [
+export const routes = [
     <Route exact path="/checkIn/:youthClubId" component={CheckInView} noLayout />,
     <Route exact path="/logbook/:youthClubId" component={LogBookView} />,
     <Route exact path="/checkIns/:youthClubId" component={LogBookListView} />,
     <Route path="/password" component={ChangePasswordView} />
+];
+
+export const superAdminRoutes = [
+    <Route path="/newSeason" component={NewSeason} />
 ];
