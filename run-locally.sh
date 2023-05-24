1de2dcc85990b90f8893ddc63bd79b023e87bc6c
Olli Helin <olli.helin@gofore.com>
2023-05-24 16:08:52 +0300

    chore: fix docker compose files for compose v2

--- run-locally.sh
+++ run-locally.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 # Use Compose V2
-# Bust cache by default. Override with a parameter.
-DOCKER_BUILDKIT=0 docker compose -f docker-compose.yml.local build --build-arg BUILD_ID=${1:-$(date +%s)}
+docker compose -f docker-compose.yml.local build
 docker compose -f docker-compose.yml.local up
