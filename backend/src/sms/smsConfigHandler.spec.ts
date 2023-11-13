b03254620c288416cc80c7cce5702c7418ecbc70
Olli Helin <olli.helin@gofore.com>
2023-11-13 13:50:55 +0200

    chore: use clearer placeholder creds

--- backend/src/sms/smsConfigHandler.spec.ts
+++ backend/src/sms/smsConfigHandler.spec.ts
@@ -7,8 +7,8 @@ describe('SMSConfig', () => {
 
   it('returns the SMS config stored in the environment', async () => {
     Object.assign(process.env, {
-      TELIA_USERNAME: 'mephisto',
-      TELIA_PASSWORD: 'hunter123',
+      TELIA_USERNAME: 'tunnus',
+      TELIA_PASSWORD: 'salasana',
       TELIA_USER: 'Vantaa',
       TELIA_ENDPOINT: 'https://api.example.com/restsms/send',
       TELIA_BATCH_ENDPOINT: 'https://api.example.com/restsms/batch/send',
@@ -17,31 +17,31 @@ describe('SMSConfig', () => {
     expect(SMSConfig.getTeliaConfig()).toEqual({
       batchEndPoint: 'https://api.example.com/restsms/batch/send',
       endPoint: 'https://api.example.com/restsms/send',
-      password: 'hunter123',
+      password: 'salasana',
       user: 'Vantaa',
-      username: 'mephisto',
+      username: 'tunnus',
     });
   });
 
   it('uses fallback values for REST endpoints', async () => {
     Object.assign(process.env, {
-      TELIA_USERNAME: 'mephisto',
-      TELIA_PASSWORD: 'hunter123',
+      TELIA_USERNAME: 'tunnus',
+      TELIA_PASSWORD: 'salasana',
       TELIA_USER: 'Vantaa',
     });
 
     expect(SMSConfig.getTeliaConfig()).toEqual({
       batchEndPoint: 'https://ws.mkv.telia.fi/restsms/lekabrest/batchsend/json',
       endPoint: 'https://ws.mkv.telia.fi/restsms/lekabrest/send',
-      password: 'hunter123',
+      password: 'salasana',
       user: 'Vantaa',
-      username: 'mephisto',
+      username: 'tunnus',
     });
   });
 
   it('returns undefined if username is not set in the environment', async () => {
     Object.assign(process.env, {
-      TELIA_PASSWORD: 'hunter123',
+      TELIA_PASSWORD: 'salasana',
       TELIA_USER: 'Vantaa',
     });
 
@@ -50,8 +50,8 @@ describe('SMSConfig', () => {
 
   it('returns undefined if user is not set in the environment', async () => {
     Object.assign(process.env, {
-      TELIA_USERNAME: 'mephisto',
-      TELIA_PASSWORD: 'hunter123',
+      TELIA_USERNAME: 'tunnus',
+      TELIA_PASSWORD: 'salasana',
     });
 
     expect(SMSConfig.getTeliaConfig()).toBe(undefined);
@@ -59,7 +59,7 @@ describe('SMSConfig', () => {
 
   it('returns undefined if password is not set in the environment', async () => {
     Object.assign(process.env, {
-      TELIA_USERNAME: 'mephisto',
+      TELIA_USERNAME: 'tunnus',
       TELIA_USER: 'Vantaa',
     });
 
